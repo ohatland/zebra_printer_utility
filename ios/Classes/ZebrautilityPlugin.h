f70e8f01924f15902184debd6b84c954035e09df
Øyvind Hatland <oyvind.hatland@yahoo.com>
2025-04-19 19:48:06 +0200

    Remove duplicate ZebrautilityPlugin.h; expose only ZebraUtilPlugin.h

--- ios/Classes/ZebrautilityPlugin.h
+++
@@ -1,9 +0,0 @@
-#import <Flutter/Flutter.h>
-#import "ZebraUtilPlugin.h"
-#import "SwiftZebraUtilPlugin-Swift.h"   //  <-- viktig for bridging
-
-@implementation ZebraUtilPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftZebraUtilPlugin registerWithRegistrar:registrar];
-}
-@end
